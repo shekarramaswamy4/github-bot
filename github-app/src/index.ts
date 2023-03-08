233999fef5116baaa870f0e885ecfc655cfa4fb7
Shekar Ramaswamy <shekar@slide.so>
2023-03-08 12:07:28 -0800

    Remove extraneous comments

--- github-app/src/index.ts
+++ github-app/src/index.ts
@@ -68,23 +68,3 @@ export default (app: Probot, { getRouter }) => {
     res.send("healthy");
   });
 };
-
-/**
-General plan:
-1. Explicitly handle PR opens
-2. Get the diff of the PR
-3. Parse the diff for files that were changed - support python only
-4. Use this https://huggingface.co/SEBIS/code_trans_t5_large_source_code_summarization_python_multitask_finetune for per file summaries
-5. Post the summary as a comment on the PR
-7. use shekar-bot on demand "review" instead of for every PR (probably just watching something) // how to @ mention bots in comments?
-
-8. create demo / diagram for how this works
-8. create plan for an enterprise deployment
-
-outstanding questions
-1. how does deployment work?
-- https://probot.github.io/docs/deployment/
-
-Known limiations
-1. cant @ mention bots in comments
- */
