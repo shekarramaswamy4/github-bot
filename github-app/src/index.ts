5facf28ec4a01210e7b7584602eb3b4c332f288b
Shekar Ramaswamy <shekar@slide.so>
2023-03-07 21:19:07 -0800

    Parsing a github diff

--- github-app/src/index.ts
+++ github-app/src/index.ts
@@ -1,11 +1,18 @@
 import { Probot } from "probot";
+import fetch from "node-fetch";
 
 // @ts-ignore
 export default (app: Probot, { getRouter }) => {
   app.on("pull_request.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this pull request!",
+      body: `Thanks for opening pull request number ${context.payload.number}!`,
     });
+
+    const diffUrl = context.payload.pull_request.diff_url;
+    let response = await fetch(diffUrl);
+    let diffData = await response.text();
+    console.log(diffData);
+
     await context.octokit.issues.createComment(issueComment);
   });
 
@@ -24,6 +31,7 @@ General plan:
 4. Use this https://huggingface.co/SEBIS/code_trans_t5_large_source_code_summarization_python_multitask_finetune for per file summaries
 5. Post the summary as a comment on the PR
 7. use shekar-bot on demand instead of for every PR (probably just watching something)
+8. create plan for an enterprise deployment
 
 outstanding questions
 1. how does deployment work?
